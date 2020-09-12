acbe8782183fad3fa8031cf6b8d7a0a20bb8cef7
Nick  Sword <nicksword@hotmail.com>
2020-09-12 17:30:30 +0800

    restructure of files. Service worker installed

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,12 +3,13 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/favicon.ico",
-  "/manifest.webmanifest",
-  "/assets/css/style.css",
-  "/assets/js/loadImages.js",
+  "/assets/css/styles.css",
+  "/assets/js/index.js",
+  "/assets/js/db.js",
   "/assets/images/icons/icon-192x192.png",
   "/assets/images/icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
 
 // install
